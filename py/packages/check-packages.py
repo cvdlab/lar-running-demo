70a77eb3ef0e876df46cb78bfa2865c627b14070
Francesco Furiani <francesco@furio.me>
2013-10-31 14:51:07 +0100

    Prerequisites

--- py/packages/check-packages.py
+++ py/packages/check-packages.py
@@ -1,13 +1,25 @@
-
-
+import sys
 
 packages = []
 packages.extend(['collections', 'scipy', 'numpy', 'pyplasm', 'time']) # LAR
 packages.extend(['sys', 'logging', 'simplejson', 'json', 'requests', 'termcolor']) # mtx-accel
 packages.extend(['png', 'matplotlib', 'struct']) # pngstack
+packages.extend(['time', 'gc']) # generic
 
+def main(argv):
+	fullpkg = True
 
-try:
-    return __import__(package)
-except ImportError:
-    return None
+	for pkg in packages:
+		try:
+			__import__(pkg)
+		except ImportError:
+			fullpkg = False
+			print "Missing package: " + pkg
+	
+	if fullpkg == False:
+		sys.exit(2)
+	
+	sys.exit(0)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ sh/prerequites.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+PYTHON=$(which python)
+CONVERT=$(which convert)
+DCM2PNM=$(which dcm2pnm)
+JAVA=$(which java)
+
+if [ -z "$PYTHON" ]; then
+    echo "Cannot find a valid python interpreter"
+	exit 1
+fi
+
+$PYTHON ../py/packages/check-packages.py
+
+if [ $? -ne 0 ]; then
+        echo "Missing python modules, install them"
+        exit 1
+fi
+
+if [ -z "$JAVA" ]; then
+    echo "Cannot find a valid java installation"
+	exit 1
+fi
+
+if [ -z "$CONVERT" ]; then
+    echo "Cannot find a valid imagemagick installation"
+	exit 1
+fi
+
+if [ -z "$DCM2PNM" ]; then
+    echo "Cannot find a valid dcm2pnm installation"
+	exit 1
+fi
+
+exit 0
